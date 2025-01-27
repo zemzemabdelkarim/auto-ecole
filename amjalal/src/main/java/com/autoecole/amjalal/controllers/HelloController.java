8d3c06b1941ca7d55b5a13a5cf133329012b5a85
zemzemabdelkarim <bdlkrmzmzm@gmail.com>
2025-01-27 14:25:26 +0100

    reset to hello page

--- amjalal/src/main/java/com/autoecole/amjalal/controllers/HelloController.java
+++ amjalal/src/main/java/com/autoecole/amjalal/controllers/HelloController.java
@@ -2,13 +2,13 @@ package com.autoecole.amjalal.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
-    @RequestMapping ("/")
+    @GetMapping("/")
     public String Hello(){
-        return "index";
+        return "hello";
     }
 }
